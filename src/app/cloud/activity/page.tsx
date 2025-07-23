7bc88f25f5ee8b276ba5ce9a1308440e3ef4978b
0x0x9 <0x00x9@gmail.com>
2025-07-23 19:34:42 +0200

    chore: push all latest changes and dependency updates

--- src/app/cloud/activity/page.tsx
+++ src/app/cloud/activity/page.tsx
@@ -2,6 +2,8 @@
 import { Suspense } from "react";
 import ActivityClient from "./client";
 
+export const dynamic = "force-dynamic";
+
 export default function CloudActivityPage() {
   return (
     <Suspense>
