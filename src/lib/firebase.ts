356998e7d04133673815e749413c35a198430c8f
noname 00X67 <00x67g@gmail.com>
2025-07-31 18:15:15 +0000

    le firebase du projet : // Import the functions you need from the SDKs y

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
-import { getAuth, type Auth, type User } from "firebase/auth";
+import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
 import { getStorage, type FirebaseStorage } from "firebase/storage";
 import { getFunctions, type Functions } from "firebase/functions";
 
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyD81xiaLYEzWZlbdHzL-BdKuj_NPUINKsE",
+  authDomain: "xyzzai.firebaseapp.com",
+  databaseURL: "https://xyzzai-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "xyzzai",
+  storageBucket: "xyzzai.appspot.com",
+  messagingSenderId: "424780446122",
+  appId: "1:424780446122:web:584566b21e5f6d8cd053d3",
+  measurementId: "G-SC8W810MLQ"
 };
 
 let app: FirebaseApp;
@@ -33,6 +34,5 @@ auth = getAuth(app);
 db = getFirestore(app);
 storage = getStorage(app);
 functions = getFunctions(app);
-
 
 export { app, auth, db, storage, functions };
